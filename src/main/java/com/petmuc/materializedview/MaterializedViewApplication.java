c289f392dc04e57f255dfa098a851f0719991a67
Peter Togara <petertogara@gmail.com>
2023-06-12 00:35:24 +0200

    Added models

--- src/main/java/com/petmuc/materializedview/MaterializedViewApplication.java
+++ src/main/java/com/petmuc/materializedview/MaterializedViewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MaterializedviewApplication {
+public class MaterializedViewApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MaterializedviewApplication.class, args);
+        SpringApplication.run(MaterializedViewApplication.class, args);
     }
 
 }
