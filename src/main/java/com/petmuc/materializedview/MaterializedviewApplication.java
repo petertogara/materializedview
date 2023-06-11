96ebb36049f44d318bd509bb26562c6644f8fa6a
Peter Togara <petertogara@gmail.com>
2023-06-12 00:02:59 +0200

    Initial commit

---
+++ src/main/java/com/petmuc/materializedview/MaterializedviewApplication.java
@@ -0,0 +1,13 @@
+package com.petmuc.materializedview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterializedviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterializedviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petmuc/materializedview/MaterializedviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmuc.materializedview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterializedviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
