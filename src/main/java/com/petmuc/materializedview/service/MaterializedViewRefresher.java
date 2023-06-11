6c70c620354e1aa86f46bbae46cd6520775d2e28
Peter Togara <petertogara@gmail.com>
2023-06-12 01:29:25 +0200

    -added MaterializedViewRefresher scheduler to call the stored procedure

---
+++ src/main/java/com/petmuc/materializedview/service/MaterializedViewRefresher.java
@@ -0,0 +1,21 @@
+package com.petmuc.materializedview.service;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class MaterializedViewRefresher {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    @Scheduled(fixedRate = 5000L)
+    public void refresh(){
+        this.entityManager.createNativeQuery("call refresh_mat_view();").executeUpdate();
+    }
+
+}
